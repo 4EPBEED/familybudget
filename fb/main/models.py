f0fae21da48a40ba6fa1c76652f58de9bc50a8e3
Adam <adampolyanskiy@gmail.com>
2021-10-11 16:36:19 +0700

    add main models and enable main app

--- fb/main/models.py
+++ fb/main/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.db.models.expressions import Value
 
 # Create your models here.
+class FamilyMember(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+class IncomeSource():
+    title = models.CharField(max_length=30)
+    desc = models.CharField(max_length=200)
+
+class Income(models.Model):
+    amount = models.FloatField()
+    desc = models.CharField(max_length=200)
+    date = models.DateField()
+    familyMember = models.ForeignKey(FamilyMember,
+                                    on_delete=models.CASCADE,
+                                    blank=True,
+                                    null=True)
+    incomeSource = models.ForeignKey(IncomeSource,
+                                    on_delete=models.CASCADE,
+                                    blank=True,
+                                    null=True)
+
+class ExpenseSource():
+    title = models.CharField(max_length=30)
+    desc = models.CharField(max_length=200)
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    desc = models.CharField(max_length=200)
+    date = models.DateField()
+    familyMember = models.ForeignKey(FamilyMember,
+                                    on_delete=models.CASCADE,
+                                    blank=True,
+                                    null=True)
+    expenseSource = models.ForeignKey(ExpenseSource,
+                                    on_delete=models.CASCADE,
+                                    blank=True,
+                                    null=True)
+
+
